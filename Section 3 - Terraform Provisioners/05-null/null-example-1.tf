dec7ddf4a7e121d580eb13b78bf8489b49c8d9c0
Sunil Thomas Mathew <smathew@hbs.edu>
2023-11-12 14:53:41 -0500

    added

---
+++ Section 3 - Terraform Provisioners/05-null/null-example-1.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  profile = "terraform"
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
+  depends_on = [null_resource.health_check]
+}
+
+
+resource "null_resource" "health_check" {
+
+ provisioner "local-exec" {
+
+    command = "curl https://google.com"
+  }
+}
